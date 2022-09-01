9b2e6f1e5b75fd1788d852325827fabd7cdd9723
Noah <45659684+NoahDrucker@users.noreply.github.com>
2022-09-01 15:08:49 +0300

    Add files via upload

---
+++ php/amazonpricing.php
@@ -0,0 +1,28 @@
+<?php
+
+$params = array(
+    'target' => 'amazon_pricing',
+    'query' => 'B09H74FXNW',
+    'parse' => True
+);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://scrape.smartproxy.com/v1/tasks');
+curl_setopt($ch, CURLOPT_USERPWD, 'SPusername' . ':' . 'SPpassword');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+
+$headers = array();
+$headers[] = 'Content-Type: application/json';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
---
+++ php/wayfair.php
@@ -0,0 +1,28 @@
+<?php
+
+$params = array(
+    'target' => 'wayfair',
+    'url' => 'https://www.wayfair.com/rugs/pdp/highland-dunes-gunnell-30-x-18-non-slip-outdoor-door-mat-w005481595.html',
+    'parse' => False
+);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://scrape.smartproxy.com/v1/tasks');
+curl_setopt($ch, CURLOPT_USERPWD, 'SPusername' . ':' . 'SPpassword');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+
+$headers = array();
+$headers[] = 'Content-Type: application/json';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
